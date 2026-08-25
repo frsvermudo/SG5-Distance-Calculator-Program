1293c506cf62addec422055cf74c61da7dd825c0
frsvermudo <frsvermudo@brc.pshs.edu.ph>
2026-08-25 08:12:34 +0800

    Update distance_calculator.py

--- distance_calculator.py
+++ distance_calculator.py
@@ -1,1 +1,12 @@
+x=float(input("Enter distance in kilometers: "))
+y=0.621371
+z=x*y
+print("Distance in miles:",z)
 
+a=input("Do you want to convert another distance? (yes/no): ")
+if a=="yes":
+    q=float(input("Enter distance in kilometers: "))
+    r=q*y
+    print("Distance in miles:",r)
+else:
+    print("Program ended.")
